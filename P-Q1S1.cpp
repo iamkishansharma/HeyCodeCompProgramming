ee9ef1dae051e233761d80d27f496c6075a87a57
Iamkishansharma <skishan781@gmail.com>
2019-10-21 16:21:42 +0530

    First Commit

---
+++ P-Q1S1.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    char no;
+    if('a'||'e'||'i'||'o'||'u'||'A'||'E'||'I'||'O'||'U'){
+        cout<<no<<" is an Vowel Letter.";
+    }
+    else{
+        cout<<no<<" is a Consonant Letter.";
+    }
+    return 0;
+}
