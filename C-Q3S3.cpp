b6860508d9a3173320414e45dc52c26214a8af7f
Iamkishansharma <skishan781@gmail.com>
2019-10-24 08:02:02 +0530

    Day3

---
+++ C-Q3S3.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+int main(){
+    int cupOf500,cupOf300,requiredWater;
+    cupOf500 = 500;
+    cupOf300 = 300;
+    requiredWater = 500-300;
+    
+    cout<<"\nThe required water is "<<requiredWater<<"ml";
+    return 0;
+}
---
+++ P-Q3S3.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+int main(){
+    char ch;
+    cout<<"Enter a Character: ";
+    cin>>ch;
+    cout<<"\nThe ASCII value of "<<ch<<" is "<<int(ch);
+    return 0;
+}
