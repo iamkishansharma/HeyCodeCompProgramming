bfaf0c1cf867bd9c5c516067c2d99e0711871dc8
Iamkishansharma <skishan781@gmail.com>
2019-10-27 10:05:59 +0530

    Day6

---
+++ C-Q6S6.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int no, i;
+    int a=-1,b=1,c;
+    cout<<"Enter a Number: ";
+    cin>>no;
+    for (i = 0; i <= no; i++) {
+        c =a+b;
+        cout<<c<<" ";
+        a = b;
+        b = c;
+    }
+    return 0;    
+}
---
+++ P-Q6S6.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+int multiply (int a, int b) {
+	if (b == 0) {
+		return 0;
+	} else if (b > 0) {
+		return (a + multiply (a, b - 1));
+	} else {
+		return (-multiply (a, -b));
+	}
+}
+
+int main(){
+	int noOfPeopleInOneDay = 1200000;
+	int oneYearDays = 365;
+	cout<<"\nThe Bus System carries "<<multiply(noOfPeopleInOneDay,oneYearDays)<<" people in One Year.";
+	return 0;
+}
