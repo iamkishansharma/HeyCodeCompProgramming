77dfb91e83ed0e839045f1c7445ed311f72aa5f1
Iamkishansharma <skishan781@gmail.com>
2019-10-29 10:55:54 +0530

    Day8

---
+++ P-Q8S8.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+int main(){
+    float b,h,r;
+    cout << "Enter Radius of a Circle: ";
+    cin>>r;
+    float areaOfCircle = r*r*3.1416;
+    cout<<"\nArea of Circle is "<<areaOfCircle;
+    cout<<"\n\nEnter Length and Width of Rectangle: ";
+    cin>>b>>h;
+    float areaOfRect = l*b;
+    cout<<"\nArea of Rectangle is "<<areaOfRect;
+    return 0;
+}
