1a6ca8516f016239f08b8c017e3bb687a06682bf
Iamkishansharma <skishan781@gmail.com>
2019-10-30 08:52:49 +0530

    Day8

--- C-Q8S8.cpp
+++ C-Q8S8.cpp
@@ -1,6 +1,13 @@
 #include<iostream>
 using namespace std;
 int main(){
-    int no;
+    int sP = 68, sC = 3;
+    int cP = 75, cC = 1;
+    int bP = 43, bC = 2;
+    int returnedBracelet = 1, rebateOnComputerGame = 10;
     
+    int totalGiftPrice = (sP*sC)+(cp*cC)+(bP*bC);
+    int discountAndRebate = (bP*returnedBracelet)+rebateOnComputerGame;
+    cout << "\nFinal Gift price is" << totalGiftPrice - discountAndRebate;
+    return 0;
 }
