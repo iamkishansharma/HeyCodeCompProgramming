f42ccdf224d99b76da2bdeee3dbc0b06efb87c72
Kishan Kr Sharma <36340195+iamkishansharma@users.noreply.github.com>
2019-11-13 23:17:09 +0530

    Day21

---
+++ P-Q21S21.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int no1, no2;
+    cin >> no1 >> no2;
+    if(no1 > no2){
+        cout << no1 << ” is greater” << endl;
+    }
+    else{
+        cout << no2 << ” is greater” << endl;
+    }
+    return 0;
+}
